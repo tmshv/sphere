f851a281f406dd83ef3c098d5b5c969140044168
Roman Timashev <roman@tmshv.ru>
2022-11-16 18:20:34 +0600

    add spotlight

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,30 @@ import App from "./App";
 import "./style.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { MantineProvider } from "@mantine/core";
+import { SpotlightProvider } from '@mantine/spotlight';
+import type { SpotlightAction } from '@mantine/spotlight';
+import { IconHome, IconDashboard, IconFileText, IconSearch } from '@tabler/icons';
+
+const actions: SpotlightAction[] = [
+  {
+    title: 'Home',
+    description: 'Get to home page',
+    onTrigger: () => console.log('Home'),
+    icon: <IconHome size={18} />,
+  },
+  {
+    title: 'Dashboard',
+    description: 'Get full information about current system status',
+    onTrigger: () => console.log('Dashboard'),
+    icon: <IconDashboard size={18} />,
+  },
+  {
+    title: 'Documentation',
+    description: 'Visit documentation to lean more about all features',
+    onTrigger: () => console.log('Documentation'),
+    icon: <IconFileText size={18} />,
+  },
+];
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -14,9 +38,18 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         xs: 4,
       }
     }}>
-      <MapProvider>
-        <App />
-      </MapProvider>
+      <SpotlightProvider
+        actions={actions}
+        searchIcon={<IconSearch size={18} />}
+        searchPlaceholder="Search..."
+        shortcut="mod + p"
+        nothingFoundMessage="Nothing found..."
+        disabled
+      >
+        <MapProvider>
+          <App />
+        </MapProvider>
+      </SpotlightProvider>
     </MantineProvider>
   </React.StrictMode>
 );
