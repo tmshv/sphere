3f81e4b984cea1ea0ab3d85aaa12773ad8b0b750
Roman Timashev <roman@tmshv.ru>
2022-11-17 01:14:11 +0600

    default style is satellite

--- src/state/map-style.ts
+++ src/state/map-style.ts
@@ -2,7 +2,7 @@ import { createMachine } from 'xstate';
 
 export const mapStyleMachine = createMachine({
     id: 'mapStyle',
-    initial: "vector",
+    initial: "satellite",
     states: {
         vector: { on: { TOGGLE: 'satellite' } },
         satellite: { on: { TOGGLE: 'vector' } },
