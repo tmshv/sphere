d2c3ff4c1c268d23c61fe8b4900d314c93e71147
Roman Timashev <roman@tmshv.ru>
2022-11-18 04:47:48 +0600

    app layout sidebars support

--- src/ui/AppLayout/index.tsx
+++ src/ui/AppLayout/index.tsx
@@ -5,7 +5,11 @@ const useStyles = createStyles(() => ({
         width: "100%",
         height: "100%",
     },
+    main: {
+        flex: 1,
+    },
     body: {
+        position: "relative",
         flex: 1,
     },
 }))
@@ -13,9 +17,11 @@ const useStyles = createStyles(() => ({
 export type AppLayoutProps = {
     children: React.ReactNode
     footer: React.ReactNode
+    leftSidebar?: React.ReactNode
+    rightSidebar?: React.ReactNode
 }
 
-export const AppLayout: React.FC<AppLayoutProps> = ({ children, footer }) => {
+export const AppLayout: React.FC<AppLayoutProps> = ({ children, footer, leftSidebar, rightSidebar }) => {
     const { classes: s } = useStyles()
 
     return (
@@ -23,9 +29,18 @@ export const AppLayout: React.FC<AppLayoutProps> = ({ children, footer }) => {
             direction="column"
             className={s.container}
         >
-            <div className={s.body}>
-                {children}
-            </div>
+            <Flex
+                direction={"row"}
+                className={s.main}
+            >
+                {leftSidebar}
+
+                <div className={s.body}>
+                    {children}
+                </div>
+
+                {rightSidebar}
+            </Flex>
 
             {footer}
         </Flex>
