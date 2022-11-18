051e151fa636880eb3e712387a5f4e46140754cc
Roman Timashev <roman@tmshv.ru>
2022-11-18 16:13:02 +0600

    sphere source refactoring

--- src/components/SphereMap/SphereSource.tsx
+++ src/components/SphereMap/SphereSource.tsx
@@ -2,14 +2,6 @@ import { ReactNode } from "react";
 import { Source } from "react-map-gl";
 import { useAppSelector } from "../../store/hooks";
 
-const pointType = new Set(["Point", "MultiPoint"])
-const lineType = new Set(["LineString", "MultiLineStreing"])
-const polygonType = new Set(["Polygon", "MultiPolygon"])
-
-// function useData(id: string) {
-
-// }
-
 export type SphereSourceProps = {
     mapId: string
     id: string
@@ -17,19 +9,15 @@ export type SphereSourceProps = {
 }
 
 export const SphereSource: React.FC<SphereSourceProps> = ({ id, mapId, children }) => {
-    // const { [mapId]: ref } = useMap()
-
     const data = useAppSelector(state => state.source.items[id].data)
 
     return (
-        <>
-            <Source
-                id={id}
-                type={"geojson"}
-                data={data}
-            >
-                {children}
-            </Source>
-        </>
+        <Source
+            id={id}
+            type={"geojson"}
+            data={data}
+        >
+            {children}
+        </Source>
     );
 }
