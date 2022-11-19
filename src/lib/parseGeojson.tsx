5ca104baf7da1369f5dca7f6a42f538cea2019fc
Roman Timashev <roman@tmshv.ru>
2022-11-20 00:23:19 +0600

    parseGeojson improved

--- src/lib/parseGeojson.tsx
+++ src/lib/parseGeojson.tsx
@@ -12,66 +12,74 @@ function isFeatureCollection(json: any): boolean {
 export async function parseGeojson(name: string, location: string, raw: string): Promise<Dataset[] | null> {
     try {
         const parsed = JSON.parse(raw)
-        if (isFeatureCollection(parsed)) {
-            const points: PointDataset = {
-                id: nextId(),
-                name,
-                location,
-                type: SourceType.Points,
-                data: [],
-            }
-            const lines: LineStringDataset = {
-                id: nextId(),
-                name,
-                location,
-                type: SourceType.Lines,
-                data: [],
-            }
-            const polygons: PolygonDataset = {
-                id: nextId(),
-                name,
-                location,
-                type: SourceType.Polygons,
-                data: [],
-            }
-
-            const features = (parsed as GeoJSON.FeatureCollection).features
-            for (const feature of features) {
-                // const id = feature.id ?? nextId()
-                const id = nextId()
-                if (pointType.has(feature.geometry.type)) {
-                    points.data.push({
-                        id,
-                        geometry: feature.geometry as any,
-                        data: feature.properties ?? {},
-                        meta: {
-                        },
-                    })
-                }
-                if (lineType.has(feature.geometry.type)) {
-                    points.data.push({
-                        id,
-                        geometry: feature.geometry as any,
-                        data: feature.properties ?? {},
-                        meta: {
-                        },
-                    })
-                }
-                if (polygonType.has(feature.geometry.type)) {
-                    points.data.push({
-                        id,
-                        geometry: feature.geometry as any,
-                        data: feature.properties ?? {},
-                        meta: {
-                        },
-                    })
-                }
-            }
-
-            return [points, lines, polygons]
-        } else {
+        if (!isFeatureCollection(parsed)) {
             return null
         }
+        const points: PointDataset = {
+            id: nextId(),
+            name,
+            location,
+            type: SourceType.Points,
+            data: [],
+        }
+        const lines: LineStringDataset = {
+            id: nextId(),
+            name,
+            location,
+            type: SourceType.Lines,
+            data: [],
+        }
+        const polygons: PolygonDataset = {
+            id: nextId(),
+            name,
+            location,
+            type: SourceType.Polygons,
+            data: [],
+        }
+
+        const features = (parsed as GeoJSON.FeatureCollection).features
+        for (const feature of features) {
+            const id = nextId()
+            if (pointType.has(feature.geometry.type)) {
+                points.data.push({
+                    id,
+                    geometry: feature.geometry as any,
+                    data: feature.properties ?? {},
+                    meta: {
+                    },
+                })
+            }
+            if (lineType.has(feature.geometry.type)) {
+                points.data.push({
+                    id,
+                    geometry: feature.geometry as any,
+                    data: feature.properties ?? {},
+                    meta: {
+                    },
+                })
+            }
+            if (polygonType.has(feature.geometry.type)) {
+                points.data.push({
+                    id,
+                    geometry: feature.geometry as any,
+                    data: feature.properties ?? {},
+                    meta: {
+                    },
+                })
+            }
+        }
+
+        const result: Dataset[] = []
+        if (points.data.length > 0) {
+            result.push(points)
+        }
+        if (lines.data.length > 0) {
+            result.push(lines)
+        }
+        if (polygons.data.length > 0) {
+            result.push(polygons)
+        }
+        return result
     } catch (error) {
         return null
     }
