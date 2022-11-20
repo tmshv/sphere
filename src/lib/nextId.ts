68240de9c4caae61ecf9bdbfbdc94011cbcabb4f
Roman Timashev <roman@tmshv.ru>
2022-11-21 05:04:30 +0600

    nextId options

--- src/lib/nextId.ts
+++ src/lib/nextId.ts
@@ -1,6 +1,9 @@
 let start = 0
 
-export function nextId(): string {
-    const id = start ++
+export function nextId(prefix?: string, value?: number): string {
+    const id = value ? value : start++
+    if (prefix) {
+        return `${prefix}-${id}`
+    }
     return `${id}`
 }
