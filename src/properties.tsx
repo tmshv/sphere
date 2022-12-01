628b943f86de28b1a85d2af1b7dfb4c620ee0d04
Roman Timashev <roman@tmshv.ru>
2022-12-02 03:17:48 +0600

    use react-table for property table

--- src/properties.tsx
+++ src/properties.tsx
@@ -3,11 +3,14 @@ import "./style.css";
 import { emit, listen, UnlistenFn } from "@tauri-apps/api/event";
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-;// emit an event that are only visible to the current window
+import { useReactTable, createColumnHelper, getCoreRowModel, flexRender } from '@tanstack/react-table'
+    ;// emit an event that are only visible to the current window
 // appWindow.emit('event', { message: 'Tauri is awesome!' })// emit an event that are only visible to the current window
 // appWindow.emit('event', { message: 'Tauri is awesome!' }) from "react-dom/client";
 import { appWindow, WebviewWindow } from '@tauri-apps/api/window'
 import { Table } from "@mantine/core";
+
+type PropertyItem = Record<string, any>
 
 function useEvent<T>(eventName: string) {
     const [payload, setPayload] = useState<T | undefined>(undefined)
@@ -32,20 +35,55 @@ function useEvent<T>(eventName: string) {
     return payload
 }
 
+const columnHelper = createColumnHelper<PropertyItem>()
+
 type PropertiesSetPayload = {
-    properties: Record<string, any>[]
+    properties: PropertyItem[]
 }
 
-function useData(): [string[], Record<string, any>[]] | undefined {
+function useData(): [any[], PropertyItem[]] | undefined {
     const data = useEvent<PropertiesSetPayload>("properties-set")
     if (!data) {
         return undefined
     }
 
-    console.log("got", data)
-
     const head = Object.keys(data.properties[0])
-    return [head, data.properties]
+    const columns = head.map(key => columnHelper.accessor(key, {
+        id: key,
+        cell: info => <i>{info.getValue()}</i>,
+        header: () => <span>{key}</span>,
+    }))
+    // const columns = [
+    // columnHelper.accessor('firstName', {
+    //     cell: info => info.getValue(),
+    //     footer: info => info.column.id,
+    // }),
+    // columnHelper.accessor(row => row.lastName, {
+    //     id: 'lastName',
+    //     cell: info => <i>{info.getValue()}</i>,
+    //     header: () => <span>Last Name</span>,
+    //     footer: info => info.column.id,
+    // }),
+    // columnHelper.accessor('age', {
+    //     header: () => 'Age',
+    //     cell: info => info.renderValue(),
+    //     footer: info => info.column.id,
+    // }),
+    // columnHelper.accessor('visits', {
+    //     header: () => <span>Visits</span>,
+    //     footer: info => info.column.id,
+    // }),
+    // columnHelper.accessor('status', {
+    //     header: 'Status',
+    //     footer: info => info.column.id,
+    // }),
+    // columnHelper.accessor('progress', {
+    //     header: 'Profile Progress',
+    //     footer: info => info.column.id,
+    // }),
+    // ]
+
+    return [columns, data.properties]
 }
 
 const App: React.FC = () => {
@@ -57,34 +95,103 @@ const App: React.FC = () => {
         <View />
     )
 }
-const View: React.FC = () => {
-    const data = useData()
-    if (!data) {
-        return null
-    }
 
-    const [head, rows] = data
+type PropertyTableProps = {
+    data: PropertyItem[]
+    columns: any[]
+}
+
+const PropertyTable: React.FC<PropertyTableProps> = ({ data, columns }) => {
+    const table = useReactTable({
+        // columns: head,
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
 
     return (
-        <Table>
+        <table>
             <thead>
-                <tr>
-                    {head.map((key, i) => (
-                        <th key={i}>{key}</th>
-                    ))}
-                </tr>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map(header => (
+                            <th key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext()
+                                    )}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
             </thead>
+
             <tbody>
-                {rows.map((row, i) => (
-                    <tr key={i}>
-                        {head.map((key, i) => (
-                            <td key={i}>{row[key]}</td>
+                {table.getRowModel().rows.map(row => (
+                    <tr key={row.id}>
+                        {row.getVisibleCells().map(cell => (
+                            <td key={cell.id}>
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </td>
                         ))}
                     </tr>
                 ))}
             </tbody>
-        </Table>
+            <tfoot>
+                {table.getFooterGroups().map(footerGroup => (
+                    <tr key={footerGroup.id}>
+                        {footerGroup.headers.map(header => (
+                            <th key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                        header.column.columnDef.footer,
+                                        header.getContext()
+                                    )}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </tfoot>
+        </table>
     )
+}
+
+const View: React.FC = () => {
+    const def = useData()
+    if (!def) {
+        return null
+    }
+
+    return (
+        <PropertyTable
+            columns={def[0]}
+            data={def[1]}
+        />
+    )
+
+    // return (
+    //     <Table>
+    //         <thead>
+    //             <tr>
+    //                 {head.map((key, i) => (
+    //                     <th key={i}>{key}</th>
+    //                 ))}
+    //             </tr>
+    //         </thead>
+    //         <tbody>
+    //             {rows.map((row, i) => (
+    //                 <tr key={i}>
+    //                     {head.map((key, i) => (
+    //                         <td key={i}>{row[key]}</td>
+    //                     ))}
+    //                 </tr>
+    //             ))}
+    //         </tbody>
+    //     </Table>
+    // )
 }
 
 // async function main() {
