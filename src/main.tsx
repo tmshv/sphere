4ff28a2ffd6f0870611487555e5ad5b35933c33c
Roman Timashev <roman@tmshv.ru>
2022-12-04 00:57:42 +0600

    eslint fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,65 +1,64 @@
-import "./style.css";
-import "mapbox-gl/dist/mapbox-gl.css";
+import "./style.css"
+import "mapbox-gl/dist/mapbox-gl.css"
 
-import { listen } from "@tauri-apps/api/event";
-import { appWindow } from '@tauri-apps/api/window';
-import { getVersion } from '@tauri-apps/api/app';
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { App } from "./components/App";
-import { actions, store } from "./store";
-import { SphereThemeProvider } from "@/components/SphereThemeProvider";
-import { isRegistered, register } from '@tauri-apps/api/globalShortcut';
+import { listen } from "@tauri-apps/api/event"
+import { appWindow } from "@tauri-apps/api/window"
+import { getVersion } from "@tauri-apps/api/app"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
+import { App } from "./components/App"
+import { actions, store } from "./store"
+import { SphereThemeProvider } from "@/components/SphereThemeProvider"
 
 async function handleHotkey() {
-  // await register('CommandOrControl+Shift+C', () => {
-  let hotkey = ""
+    // await register('CommandOrControl+Shift+C', () => {
+    let hotkey = ""
 
-  // hotkey = 'CommandOrControl+Z'
-  // if (!await isRegistered(hotkey)) {
-  //   await register('CommandOrControl+Z', () => {
-  //     console.log('undo');
-  //     store.dispatch(actions.undo())
-  //   })
-  // }
+    // hotkey = 'CommandOrControl+Z'
+    // if (!await isRegistered(hotkey)) {
+    //   await register('CommandOrControl+Z', () => {
+    //     console.log('undo');
+    //     store.dispatch(actions.undo())
+    //   })
+    // }
 
-  // hotkey = 'CommandOrControl+Shift+Z'
-  // if (!await isRegistered(hotkey)) {
-  //   await register('CommandOrControl+Shift+Z', () => {
-  //     console.log('redo');
-  //     store.dispatch(actions.redo())
-  //   })
-  // }
+    // hotkey = 'CommandOrControl+Shift+Z'
+    // if (!await isRegistered(hotkey)) {
+    //   await register('CommandOrControl+Shift+Z', () => {
+    //     console.log('redo');
+    //     store.dispatch(actions.redo())
+    //   })
+    // }
 }
 
 async function handleTheme() {
-  const theme = await appWindow.theme();
-  if (theme) {
-    store.dispatch(actions.app.setDarkTheme(theme === "dark"))
-  }
+    const theme = await appWindow.theme()
+    if (theme) {
+        store.dispatch(actions.app.setDarkTheme(theme === "dark"))
+    }
 
-  const e = "tauri://theme-changed"
-  const unlisten = await listen(e, (event) => {
-    const theme = event.payload as string
-    store.dispatch(actions.app.setDarkTheme(theme === "dark"))
-  })
+    const e = "tauri://theme-changed"
+    const unlisten = await listen(e, (event) => {
+        const theme = event.payload as string
+        store.dispatch(actions.app.setDarkTheme(theme === "dark"))
+    })
 }
 
 async function handleVersion() {
-  const version = await getVersion();
-  store.dispatch(actions.app.setVersion(version))
+    const version = await getVersion()
+    store.dispatch(actions.app.setVersion(version))
 }
 
 async function main() {
-  const e = "tauri://file-drop"
-  // const e = "tauri://file-drop-hover"
-  // const e = "tauri://file-drop-cancelled"
+    const e = "tauri://file-drop"
+    // const e = "tauri://file-drop-hover"
+    // const e = "tauri://file-drop-cancelled"
 
-  const unlisten = await listen(e, (event) => {
-    const files = event.payload as string[]
-    store.dispatch(actions.source.addFromFiles(files))
-  })
+    const unlisten = await listen(e, (event) => {
+        const files = event.payload as string[]
+        store.dispatch(actions.source.addFromFiles(files))
+    })
 }
 main()
 handleTheme()
@@ -67,11 +66,11 @@ handleVersion()
 handleHotkey()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <SphereThemeProvider>
-        <App />
-      </SphereThemeProvider>
-    </Provider>
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <Provider store={store}>
+            <SphereThemeProvider>
+                <App />
+            </SphereThemeProvider>
+        </Provider>
+    </React.StrictMode>
+)
