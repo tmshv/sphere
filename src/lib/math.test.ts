f7e50650a8873c8f7da0acfa7f04cad3c258387f
Roman Timashev <roman@tmshv.ru>
2023-04-26 00:50:20 +0530

    setup jest

---
+++ src/lib/math.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from "@jest/globals"
+import { lerp } from "./math"
+
+describe("lib/math/lerp", () => {
+    test("should return proper value", () => {
+        expect(lerp(0.5, 0, 1, 10, 20)).toEqual(15)
+        expect(lerp(0.25, 0, 1, 100, 200)).toEqual(125)
+    })
+
+    test("should return A if v=min", () => {
+        const A = Math.random()
+        const B = A + Math.random()
+        expect(lerp(1, 1, 2, A, B)).toEqual(A)
+    })
+
+    test("should return B if v=max", () => {
+        const A = Math.random()
+        const B = A + Math.random()
+        expect(lerp(2, 1, 2, A, B)).toEqual(B)
+    })
+})
