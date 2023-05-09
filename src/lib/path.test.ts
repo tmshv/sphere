26dac0db9962089a984ff8af5a60ad2c1dc617cf
Roman Timashev <roman@tmshv.ru>
2023-05-10 00:36:28 +0530

    getStem improved

--- src/lib/path.test.ts
+++ src/lib/path.test.ts
@@ -6,11 +6,27 @@ describe("getStem function", () => {
         expect(getStem("")).toBe(null)
     })
 
+    test("returns the correct stem of file if input is file.ext", () => {
+        expect(getStem("image.png")).toBe("image")
+    })
+
+    test("returns the correct stem of file if input is ./file.ext", () => {
+        expect(getStem("my-notes.docx")).toBe("my-notes")
+    })
+
     test("returns the correct stem for a file with a single extension", () => {
         expect(getStem("path/to/file.txt")).toBe("file")
     })
 
     test("returns the correct stem for a file with multiple extensions", () => {
         expect(getStem("path/to/file.min.js")).toBe("file.min")
+    })
+
+    test("returns the correct stem for a file with space in filename", () => {
+        expect(getStem("path/to/file with space.jpg")).toBe("file with space")
+    })
+
+    test("returns the correct stem for a file with no ext", () => {
+        expect(getStem("path/to/no_ext")).toBe("no_ext")
     })
 })
--- src/lib/path.ts
+++ src/lib/path.ts
@@ -6,10 +6,6 @@ export function getStem(pathname: string): string | null {
     }
 
     const parts = pathname.split("/")
-    if (parts.length === 0) {
-        return null
-    }
-
     const file = parts[parts.length - 1]
     const fileParts = file.split(".")
     if (fileParts.length === 1) {
