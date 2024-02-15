27f4c33088eea74681de85ceaeeeb1e976a66cec
Roman Timashev <roman@tmshv.ru>
2024-02-15 15:31:42 +0300

    property viewer checkUrls behavior

--- src/ui/PropertiesViewer/index.tsx
+++ src/ui/PropertiesViewer/index.tsx
@@ -1,7 +1,9 @@
-import { createStyles } from "@mantine/core"
+import { isUrl } from "@/lib/predict-data-type"
+import { createStyles, Image } from "@mantine/core"
 
 const useStyles = createStyles(theme => ({
     table: {
+        minWidth: 300,
         width: "100%",
     },
     key: {
@@ -10,15 +12,16 @@ const useStyles = createStyles(theme => ({
         display: "flex",
     },
     value: {
-        maxWidth: 150,
+        maxWidth: 200,
     },
 }))
 
 export type PropertiesViewerProps = {
     properties: { key: string, value: any }[]
+    checkUrls?: boolean
 }
 
-export const PropertiesViewer: React.FC<PropertiesViewerProps> = ({ properties }) => {
+export const PropertiesViewer: React.FC<PropertiesViewerProps> = ({ properties, checkUrls = false }) => {
     const { classes: s } = useStyles()
 
     return (
@@ -28,11 +31,17 @@ export const PropertiesViewer: React.FC<PropertiesViewerProps> = ({ properties }
                     <tr key={key}>
                         <td className={s.key}>{key}</td>
                         <td className={s.value}>
-                            {/* <Badge
-                            radius={"xs"}
-                        > */}
-                            {value}
-                            {/* </Badge> */}
+                            {checkUrls && isUrl(value) ? (
+                                <Image
+                                    src={value}
+                                    width={120}
+                                    height={120}
+                                />
+                            ) : (
+                                <span>
+                                    {value}
+                                </span>
+                            )}
                         </td>
                     </tr>
                 ))}
