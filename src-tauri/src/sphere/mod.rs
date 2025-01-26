d62552f53e2127b213d6c0c209cef48dc4632b8c
Roman Timashev <roman@tmshv.ru>
2025-01-26 15:39:05 +0300

    format

--- src-tauri/src/sphere/mod.rs
+++ src-tauri/src/sphere/mod.rs
@@ -1,12 +1,11 @@
+pub mod csv;
 pub mod geojson;
+pub mod gpx;
 pub mod mbtiles;
-pub mod tilejson;
 pub mod shape;
 pub mod source;
-pub mod csv;
-pub mod gpx;
+pub mod tilejson;
 
 pub trait Bounds {
     fn get_bounds(&self) -> Option<(f64, f64, f64, f64)>;
 }
-
