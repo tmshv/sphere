9a4c840e5b6ffcc8fbd4851f76c9d8a0c41e55b2
Roman Timashev <roman@tmshv.ru>
2025-04-27 00:06:38 +0300

    fix use draw control hook by using typing from mapbox lib

--- src/hooks/useDrawControl.ts
+++ src/hooks/useDrawControl.ts
@@ -1,7 +1,8 @@
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css"
 
 import MapboxDraw from "@mapbox/mapbox-gl-draw"
-import { useControl, useMap } from "react-map-gl/maplibre"
+import { useMap } from "react-map-gl/maplibre"
+import { useControl } from "react-map-gl/mapbox"
 import type { ControlPosition } from "react-map-gl/maplibre"
 import { useCallback, useEffect } from "react"
 
