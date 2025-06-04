85c5238bc471a1c23d07548768a8d0fd9ded651c
Roman Timashev <roman@tmshv.ru>
2025-06-05 02:41:22 +0300

    auto layer type for geojson

--- src/store/listeners/add-blank-layer.ts
+++ src/store/listeners/add-blank-layer.ts
@@ -5,6 +5,7 @@ import { nextColor } from "@/lib/color-scheme"
 import { actions } from "../actions"
 import { RootState } from ".."
 import predictLayerType from "@/lib/predict-layer-type"
+import { createSourceMetadataFromFeatureCollection } from "@/lib/source-metadata"
 
 const listener = createListenerMiddleware()
 listener.startListening({
@@ -15,7 +16,7 @@ listener.startListening({
         const layerId = nextId("layer")
         const source = sourceId
             ? state.source.items[sourceId]
-            : null
+            : undefined
         const name = source
             ? source.name
             : "Layer"
@@ -51,11 +52,16 @@ listener.startListening({
                     }))
                 }
             }
-            // TODO: use smart prediction instead of just Point
-            listenerApi.dispatch(actions.layer.setType({
-                id: layerId,
-                type: LayerType.Point,
-            }))
+
+            // predict default layer view for GeoJSON
+            if (source.type === SourceType.Geojson && source.dataset) {
+                const meta = createSourceMetadataFromFeatureCollection(source.dataset)
+                const layerType = predictLayerType(meta)
+                listenerApi.dispatch(actions.layer.setType({
+                    id: layerId,
+                    type: layerType,
+                }))
+            }
         }
 
         listenerApi.dispatch(actions.selection.selectLayer({
