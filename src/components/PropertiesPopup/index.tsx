83d0aa46be336241634be298615f3351912e9f0c
Roman Timashev <roman@tmshv.ru>
2025-06-06 01:51:41 +0300

    properties popup support may features

--- src/components/PropertiesPopup/index.tsx
+++ src/components/PropertiesPopup/index.tsx
@@ -6,26 +6,28 @@ import { Overlay } from "@/ui/Overlay"
 
 export default function PropertiesPopup() {
     const props = useAppSelector(selectProperties)
+    if (!props) {
+        return null
+    }
 
     return (
-        <>
-            {!props ? null : (
-                <Overlay topRight={(
-                    <Container pt={"lg"} style={{
-                        minWidth: 300,
-                        overflow: "hidden",
-                    }}>
-                        <Paper p={"sm"}>
-                            <Title order={3}>
-                                Properties
-                            </Title>
-                            <PropertiesViewer
-                                properties={props[0]}
-                            />
-                        </Paper>
-                    </Container>
-                )} />
-            )}
-        </>
+        <Overlay topRight={(
+            <Container pt={"lg"} style={{
+                minWidth: 300,
+                overflow: "hidden",
+            }}>
+                <Paper p={"sm"}>
+                    <Title order={3}>
+                        Properties
+                    </Title>
+                    {props.map((x, i) => (
+                        <PropertiesViewer
+                            key={i}
+                            properties={x}
+                        />
+                    ))}
+                </Paper>
+            </Container>
+        )} />
     )
 }
