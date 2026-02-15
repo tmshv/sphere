c068752e89975c21202c0fcf23f27a0b22bdfee7
Roman Timashev <roman@tmshv.ru>
2026-02-15 23:07:41 +0700

    Fix __publicField error in maplibre (#103)
    * add trailing comma
    
    * target stick to es2022
    
    * add ci test
    
    * improve bug description

--- vite.config.ts
+++ vite.config.ts
@@ -37,9 +37,14 @@ export default defineConfig({
     // to make use of `TAURI_DEBUG` and other env variables
     // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
     envPrefix: ["VITE_", "TAURI_"],
+    optimizeDeps: {
+        esbuildOptions: {
+            target: "es2022",
+        },
+    },
     build: {
         // Tauri supports es2021
-        target: ["es2021", "chrome100", "safari13"],
+        target: ["es2022", "safari15"],
         // don't minify for debug builds
         minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
         // produce sourcemaps for debug builds
