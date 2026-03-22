7473a33969b37840c93a051fcf2ff4abdeaa61da
Roman Timashev <roman@tmshv.ru>
2026-03-22 20:13:09 +0300

    use feature properties hook performance improvement

--- src/sphere-hooks/useFeatureProperties.ts
+++ src/sphere-hooks/useFeatureProperties.ts
@@ -3,9 +3,11 @@ import { selectors } from "@/store"
 import { useAppSelector } from "@/store/hooks"
 import type { MapRef } from "react-map-gl/maplibre"
 
+const EMPTY: string[] = []
+
 export default function useFeatureProperties(ref: MapRef | undefined, delay: number) {
     const layerId = useAppSelector(selectors.selection.currentLayerId)
     const previewLayerIds = useAppSelector(selectors.preview.layerIds)
-    const effectiveLayerIds = previewLayerIds.length > 0 || !layerId ? [] : [layerId]
+    const effectiveLayerIds = previewLayerIds.length > 0 || !layerId ? EMPTY : [layerId]
     useFP(ref, effectiveLayerIds, delay)
 }
